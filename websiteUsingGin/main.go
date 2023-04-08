8df43c32e3581f962aa84bbb666aff354804c583
Ganesh R <116338267+rganes5@users.noreply.github.com>
2023-04-08 10:31:04 +0530

    second commit

---
+++ websiteUsingGin/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+var userDataBase = map[string]string{
+	"Ganesh": "Ganesh@123",
+	"Stebin": "Stebin@123",
+	"Edwin":  "Edwin@123",
+}
+
+var tmpl *template.Template
+
+func init() {
+	tmpl = template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func main() {
+	r := gin.Default()
+
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	// For accessing all the assets like css and images.
+	r.Static("/assets", "./assets")
+
+	// Handle fn
+	r.GET("/", homeHandler)
+	r.POST("/login", loginCheckHandler)
+	r.GET("/welcome", welcomeHandler)
+	r.GET("/logout", logoutHandler)
+
+	r.Run(":9191")
+}
+
+func homeHandler(c *gin.Context) {
+	session := sessions.Default(c)
+	userName := session.Get("userName")
+	if userName != nil {
+		data := map[string]interface{}{
+			"userName": userName,
+		}
+		tmpl.ExecuteTemplate(c.Writer, "login.html", data)
+	} else {
+		tmpl.ExecuteTemplate(c.Writer, "index.html", nil)
+	}
+}
+
+func loginCheckHandler(c *gin.Context) {
+	session := sessions.Default(c)
+	userName := session.Get("userName")
+	if userName != nil {
+		data := map[string]interface{}{
+			"userName": userName,
+		}
+		tmpl.ExecuteTemplate(c.Writer, "login.html", data)
+	} else {
+		userName := c.PostForm("name")
+		password := c.PostForm("password")
+
+		if userDataBase[userName] == password && password != "" {
+			session.Set("userName", userName)
+			session.Save()
+
+			c.Redirect(http.StatusSeeOther, "/welcome")
+		} else {
+			data := map[string]interface{}{
+				"error": "Invalid username and password",
+			}
+			tmpl.ExecuteTemplate(c.Writer, "index.html", data)
+		}
+	}
+}
+
+func welcomeHandler(c *gin.Context) {
+	session := sessions.Default(c)
+	userName := session.Get("userName")
+	if userName != nil {
+		data := map[string]interface{}{
+			"userName": userName,
+		}
+		tmpl.ExecuteTemplate(c.Writer, "login.html", data)
+	} else {
+		c.Redirect(http.StatusSeeOther, "/")
+	}
+}
+
+func logoutHandler(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Clear()
+	session.Save()
+	c.Redirect(http.StatusSeeOther, "/")
+}
